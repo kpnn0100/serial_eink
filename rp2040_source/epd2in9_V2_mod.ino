218b566b6c8531695d3707a6608bb60e5afc03e9
kpnn <110481928+kpnn0100@users.noreply.github.com>
2023-12-13 22:02:47 +0700

    first commit

---
+++ rp2040_source/epd2in9_V2_mod.ino
@@ -0,0 +1,68 @@
+/**
+ *  @filename   :   epd2in9_V2-demo.ino
+ *  @brief      :   2.9inch e-paper V2 display demo
+ *  @author     :   Yehui from Waveshare
+ *
+ *  Copyright (C) Waveshare     Nov 09 2020
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documnetation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to  whom the Software is
+ * furished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS OR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+//#include <SPI.h>
+#include "epd2in9_V2.h"
+#include "epdpaint.h"
+#include "imagedata.h"
+
+#define COLORED     0
+#define UNCOLORED   1
+
+/**
+  * Due to RAM not enough in Arduino UNO, a frame buffer is not allowed.
+  * In this case, a smaller image buffer is allocated and you have to 
+  * update a partial display several times.
+  * 1 byte = 8 pixels, therefore you have to set 8*N pixels at a time.
+  */
+unsigned char image[1024];
+Paint paint(image, 0, 0);    // width should be the multiple of 8 
+Epd epd;
+unsigned long time_start_ms;
+unsigned long time_now_s;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("init");
+  if (epd.Init() != 0) {
+      Serial.print("e-Paper init failed ");
+      return;
+  }
+
+  epd.SetFrameMemory_Base(IMAGE_DATA);
+  epd.DisplayFrame();
+
+  time_start_ms = millis();
+}
+
+void loop() {
+  if (Serial.available())
+  epd.SetFrameMemory_Base(IMAGE_DATA);
+  epd.DisplayFrame();
+
+  // delay(300);
+}
